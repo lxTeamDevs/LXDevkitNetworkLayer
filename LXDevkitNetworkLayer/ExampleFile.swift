1a414fa7de08872f7624e37d5ad8b6e74b67e112
Artak Gevorgyan <artak@helix.am>
2023-06-27 11:19:13 +0400

    Added test cases

---
+++ LXDevkitNetworkLayer/ExampleFile.swift
@@ -0,0 +1,22 @@
+//
+//  ExampleFile.swift
+//  LXDevkitNetworkLayer
+//
+//  Created by Artak Gevorgyan on 27.06.23.
+//
+
+import Foundation
+
+public final class FrameworkClass {
+
+    let name = "FrameworkClass"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- LXDevkitNetworkLayerTests/LXDevkitNetworkLayerTests.swift
+++ LXDevkitNetworkLayerTests/LXDevkitNetworkLayerTests.swift
@@ -7,17 +7,28 @@
 
 import XCTest
 @testable import LXDevkitNetworkLayer
+//@testable import FrameworkClass
 
 final class LXDevkitNetworkLayerTests: XCTestCase {
-
+    
+    var fwkClass: FrameworkClass!
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func setUp() {
+        fwkClass = FrameworkClass()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
+    func testAdd() {
+        XCTAssertEqual(fwkClass.add(a: 1, b: 1), 2)
+    }
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
