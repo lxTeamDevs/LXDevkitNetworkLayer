529b1f8e2fe4bcf04bda717774e2f35d0271b894
Artak Gevorgyan <artak@helix.am>
2023-06-27 10:48:49 +0400

    Initial commit

---
+++ LXDevkitNetworkLayer/LXDevkitNetworkLayer.h
@@ -0,0 +1,18 @@
+//
+//  LXDevkitNetworkLayer.h
+//  LXDevkitNetworkLayer
+//
+//  Created by Artak Gevorgyan on 26.06.23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LXDevkitNetworkLayer.
+FOUNDATION_EXPORT double LXDevkitNetworkLayerVersionNumber;
+
+//! Project version string for LXDevkitNetworkLayer.
+FOUNDATION_EXPORT const unsigned char LXDevkitNetworkLayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LXDevkitNetworkLayer/PublicHeader.h>
+
+
